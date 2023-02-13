8644cfba7086b018c68ba3be1f7328795db115a5
RichieJJ <112990571+RichieJJ@users.noreply.github.com>
2023-02-13 05:07:23 -0500

    Update loadbalancer.tf

--- Rearc-Stage-02/loadbalancer.tf
+++ Rearc-Stage-02/loadbalancer.tf
@@ -25,7 +25,7 @@ resource "aws_alb_target_group" "rearc-target-group" {
   depends_on = [aws_alb.rearc-alb]
 }
 
-resource "aws_acm_certificate" "rearc-ssl-cert" {
+resource "aws_acm_certificate" "myrichmondlaw.com" {                         # "rearc-ssl-cert" {
   domain_name       = "myrichmondlaw.com"
   validation_method = "DNS"
 
@@ -34,12 +34,6 @@ resource "aws_acm_certificate" "rearc-ssl-cert" {
     validation_domain = "myrichmondlaw.com"
   }
 }
-
-# resource "aws_iam_server_certificate" "rearc-ssl-cert" {
-#   name = "rearc-ssl-cert"
-#   certificate_body = file("self_signed_ssl_cert/public.pem")
-#   private_key = file("self_signed_ssl_cert/private.pem")
-# }
 
 resource "aws_alb_listener" "rearc-http-listener" {
   load_balancer_arn = aws_alb.rearc-alb.arn
@@ -56,7 +50,7 @@ resource "aws_alb_listener" "rearc-http-listener" {
     }
 
     target_group_arn = aws_alb_target_group.rearc-target-group.arn
-    type             = "forward"
+#    type             = "forward"
   }
 }
 
